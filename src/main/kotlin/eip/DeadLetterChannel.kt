1ff15b9cac8a96aba88a37a382877502fe9ed54c
shaolang <shaolang@ymail.com>
2020-05-30 22:27:54 +0800

    Add the missing DeadLetterChannel source file

---
+++ src/main/kotlin/eip/DeadLetterChannel.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 Shaolang Ai
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package eip
+
+import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.impl.DefaultCamelContext
+
+class NoOp
+
+fun main() {
+    val context = DefaultCamelContext()
+    val directURI = "direct:dlc"
+
+    context.addRoutes(object: RouteBuilder() {
+        override fun configure() {
+            val dlq = deadLetterChannel("activemq:queue:dead")
+                .maximumRedeliveries(3)
+                .redeliveryDelay(500)
+
+            errorHandler(dlq)
+            from(directURI).bean(NoOp::class.java).to("stream:out")
+        }
+    })
+
+    context.start()
+
+    val producer = context.createProducerTemplate()
+    producer.sendBody(directURI, "Dead on arrival")
+
+    Thread.sleep(100)
+    context.stop()
+}
